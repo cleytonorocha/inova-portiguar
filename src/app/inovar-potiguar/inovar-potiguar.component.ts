0ede14bd0ad8e4c3d2f847b50a538d6491c08c01
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-05-26 14:59:44 -0300

    feat: history component

--- src/app/inovar-potiguar/inovar-potiguar.component.ts
+++ src/app/inovar-potiguar/inovar-potiguar.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../home/footer/footer.component';
 import { NavBarComponent } from '../home/nav-bar/nav-bar.component';
+import { HistoryComponent } from "../inovarPotiguar/history/history.component";
 
 @Component({
-  selector: 'app-inovar-potiguar',
-  standalone: true,
-  imports: [
-    NavBarComponent,
-    FooterComponent
-  ],
-  templateUrl: './inovar-potiguar.component.html',
-  styleUrl: './inovar-potiguar.component.css'
+    selector: 'app-inovar-potiguar',
+    standalone: true,
+    templateUrl: './inovar-potiguar.component.html',
+    styleUrl: './inovar-potiguar.component.css',
+    imports: [
+        NavBarComponent,
+        FooterComponent,
+        HistoryComponent
+    ]
 })
 export default class InovarPotiguarComponent {
 
